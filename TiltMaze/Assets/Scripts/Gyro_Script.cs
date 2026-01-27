af1a2a5186d7ae5d100fa396300055a5a845a9a0
MA. ELYSSA REESE MA�GANTI DIO <1010290@cc.usls.edu>
2026-01-26 18:29:47 -1200

    Setting Up Gyroscope

---
+++ TiltMaze/Assets/Scripts/Gyro_Script.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Gyro_Script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //to enable gyroscope on Android
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.gyro.attitude);
+        transform.rotation = Input.gyro.attitude;
+    }
+}
